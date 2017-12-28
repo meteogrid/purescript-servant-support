d98ea2deab5f39a463780560ccdce162b71bfb5c
Alberto Valverde <alberto@toscat.net>
2017-12-28 15:57:40 +0100

    use purescript-foreign-generic instead of argonaut

--- src/Servant/PureScript/Util.purs
+++ src/Servant/PureScript/Util.purs
@@ -2,12 +2,15 @@ module Servant.PureScript.Util where
 
 import Prelude
 import Control.Monad.Error.Class (class MonadError, throwError)
-import Data.Argonaut.Core (Json)
-import Data.Argonaut.Parser (jsonParser)
+import Control.Monad.Except (runExcept)
+import Data.Foreign.Generic.Class (class GenericDecode, class GenericEncode)
+import Data.Foreign.JSON (parseJSON)
 import Data.Bifunctor (lmap)
 import Data.Either (Either(..))
 import Data.Foldable (intercalate)
-import Data.Generic (class Generic)
+import Data.Foreign (Foreign)
+import Data.Generic.Rep (class Generic)
+import Global.Unsafe (unsafeStringify)
 import Network.HTTP.Affjax (AffjaxResponse)
 import Network.HTTP.StatusCode (StatusCode(..))
 import Servant.PureScript.Affjax (makeAjaxError, AjaxError, ErrorDescription(DecodingError, ParsingError, UnexpectedHTTPStatus), AjaxRequest)
@@ -17,37 +20,39 @@ import Servant.PureScript.Settings (SPSettings_(SPSettings_), SPSettingsToUrlPie
 -- |
 -- | Reports an error if status code is non success or decoding fails. The taken AjaxRequest is only for error reporting.
 getResult
-  :: forall a m
-   . Generic a
+  :: forall a m rep
+   . Generic rep a
+  => GenericDecode rep
   => MonadError AjaxError m
   => AjaxRequest
-  -> (Json -> Either String a)
+  -> (Foreign -> Either String a)
   -> AffjaxResponse String -> m a
 getResult req' decode resp = do
   let stCode = case resp.status of StatusCode code -> code
   fVal <- if stCode >= 200 && stCode < 300
             then pure resp.response
             else throwError $ makeAjaxError req' (UnexpectedHTTPStatus resp)
-  jVal <- throwLeft <<< lmap (reportRequestError req' ParsingError fVal) <<< jsonParser $ fVal
-  throwLeft <<< lmap (reportRequestError req' DecodingError (show jVal)) <<< decode $ jVal
+  jVal <- throwLeft <<< lmap (reportRequestError req' ParsingError fVal)
+                    <<< lmap show <<< runExcept <<< parseJSON $ fVal
+  throwLeft <<< lmap (reportRequestError req' DecodingError (unsafeStringify jVal)) <<< decode $ jVal
 
 throwLeft :: forall a e m. MonadError e m => Either e a -> m a
 throwLeft (Left e) = throwError e
 throwLeft (Right a) = pure a
 
 
-encodeListQuery :: forall a b. Generic a => SPSettings_ b -> String -> Array a -> String
+encodeListQuery :: forall a b rep. Generic a rep => GenericEncode rep => SPSettings_ b -> String -> Array a -> String
 encodeListQuery opts'@(SPSettings_ opts) fName = intercalate "&" <<< map (encodeQueryItem opts' fName)
 
 -- | The given name is assumed to be already escaped.
-encodeQueryItem :: forall a b. Generic a => SPSettings_ b -> String -> a -> String
+encodeQueryItem :: forall a b rep. Generic a rep => GenericEncode rep => SPSettings_ b -> String -> a -> String
 encodeQueryItem opts'@(SPSettings_ opts) fName val = fName <> "=" <> encodeURLPiece opts' val
 
 -- | Call opts.toURLPiece and encode the resulting string with encodeURIComponent.
-encodeURLPiece :: forall a params. Generic a => SPSettings_ params -> a -> String
+encodeURLPiece :: forall a rep params. Generic a rep => GenericEncode rep => SPSettings_ params -> a -> String
 encodeURLPiece (SPSettings_ opts) = case opts.toURLPiece of SPSettingsToUrlPiece_ f -> f
 
-encodeHeader :: forall a params. Generic a => SPSettings_ params -> a -> String
+encodeHeader :: forall a params rep. Generic a rep => GenericEncode rep => SPSettings_ params -> a -> String
 encodeHeader (SPSettings_ opts) = case opts.encodeHeader of SPSettingsEncodeHeader_ f -> f
 
 reportRequestError :: AjaxRequest -> (String -> ErrorDescription) -> String -> String -> AjaxError
